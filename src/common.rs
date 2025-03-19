f5e8f7ebc61f97681e8f6781a9415b57d275b923
liuwei <liuweiokay@outlook.com>
2025-03-19 14:38:47 +0800

    添加api参数

--- src/common.rs
+++ src/common.rs
@@ -940,7 +940,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://api.ione.xin:6443".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
